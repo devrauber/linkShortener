c001dc0a91bb2c3d7c2e066bee10d66e0380a4c0
Tullio Rauber <tullio.rauber@mksolutions.com.br>
2023-04-27 23:45:44 -0300

    feat(Encurtador de link): :sparkles: Encurtador de links com campo customizavel
    Encurtador de links com campo customizavel totalmente responsivo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,106 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import axios from "axios";
+import { Copy } from "@phosphor-icons/react";
+import Head from "next/head";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [url, setUrl] = useState("");
+  const [customUrl, setCustomUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const ApiKey = "b381974fc9a1f9869080032425bee2de86753581";
+
+  const generateShortUrl = async () => {
+    if (url.includes("https://") || url.includes("http://")) {
+      await axios
+        .post(
+          "https://api-ssl.bitly.com/v4/shorten",
+          {
+            long_url: url,
+            custom_bitlink: customUrl,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${ApiKey}`,
+            },
+          }
+        )
+        .then((response) => {
+          setShortUrl(response.data.link);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  function handleCopyButtonClick() {
+    navigator.clipboard.writeText(shortUrl);
+    toast.success("Valor copiado com sucesso!");
+  }
+
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-rose-800">
+      <ToastContainer />
+      <div className="flex flex-col items-center w-full mb-4 text-container">
+        <h1 className="text-8xl font-bold mb-28 text-white">
+          t<span className="text-green-500">Url</span>l.io
+        </h1>
+      </div>
+      <div className="flex flex-col items-center text-center w-full mb-4">
+        <label htmlFor="input1" className="mb-2 font-bold text-lg text-white">
+          Link para ser encurtado
+        </label>
+        <input
+          type="text"
+          id="input1"
+          className="w-3/5 border border-gray-400 p-2 rounded-md text-rose-950"
+          onChange={(e) => setUrl(e.target.value)}
+          value={url}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col items-center text-center justify-center w-full mb-4">
+        <label htmlFor="input2" className="mb-2 font-bold text-lg text-white">
+          Personalização do link (opcional)
+        </label>
+        <input
+          type="text"
+          id="input2"
+          className="border w-1/5  border-gray-400 p-2 rounded-md text-rose-950"
+          value={customUrl}
+          onChange={(e) => setCustomUrl(e.target.value)}
+        />
       </div>
-    </main>
-  )
+      <button
+        type="button"
+        onClick={() => {
+          generateShortUrl();
+        }}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mb-10 rounded"
+      >
+        Continuar
+      </button>
+      <div className="w-5/6 bg-gray-100 p-4 rounded-lg flex items-center gap-4 justify-center">
+        <input
+          type="text"
+          value={shortUrl}
+          onChange={(e) => {
+            setShortUrl(e.target.value);
+          }}
+          className="p-4 rounded-lg w-full text-slate-950"
+          readOnly
+        />
+        <div className="flex items-center justify-center">
+          <Copy
+            size={32}
+            color="rgb(34 197 94)"
+            className="cursor-pointer hover:scale-110 transition-all"
+            onClick={handleCopyButtonClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
